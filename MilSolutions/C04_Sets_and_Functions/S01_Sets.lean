69652e532aa9069a6b78ea522f6f256724e1e06b
Benjamin F Jones <benjaminfjones@gmail.com>
2024-08-10 16:26:53 -0700

    Add solutions for C04 S01_Sets

--- MilSolutions/C04_Sets_and_Functions/S01_Sets.lean
+++ MilSolutions/C04_Sets_and_Functions/S01_Sets.lean
@@ -1,6 +1,6 @@
 import Mathlib.Data.Set.Lattice
 import Mathlib.Data.Nat.Prime.Basic
-import MIL.Common
+import MilSolutions.Common
 
 section
 variable {α : Type*}
@@ -24,27 +24,33 @@ example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := by
   exact ⟨h xsu.1, xsu.2⟩
 
 example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u :=
-  fun x ⟨xs, xu⟩ ↦ ⟨h xs, xu⟩
+  fun _x ⟨xs, xu⟩ ↦ ⟨h xs, xu⟩
 
 example : s ∩ (t ∪ u) ⊆ s ∩ t ∪ s ∩ u := by
   intro x hx
   have xs : x ∈ s := hx.1
   have xtu : x ∈ t ∪ u := hx.2
-  rcases xtu with xt | xu
+  rcases xtu with xt | xu  -- ∪ is defined as a disjunction
   · left
     show x ∈ s ∩ t
     exact ⟨xs, xt⟩
-  . right
+  · right
     show x ∈ s ∩ u
     exact ⟨xs, xu⟩
 
 example : s ∩ (t ∪ u) ⊆ s ∩ t ∪ s ∩ u := by
   rintro x ⟨xs, xt | xu⟩
   · left; exact ⟨xs, xt⟩
-  . right; exact ⟨xs, xu⟩
+  · right; exact ⟨xs, xu⟩
 
 example : s ∩ t ∪ s ∩ u ⊆ s ∩ (t ∪ u) := by
-  sorry
+  intro x hx
+  rcases hx with hxt | hxu
+  case inl =>
+    exact ⟨ hxt.1, Or.inl hxt.2 ⟩
+  case inr =>
+    exact ⟨ hxu.1, Or.inr hxu.2 ⟩
+
 example : (s \ t) \ u ⊆ s \ (t ∪ u) := by
   intro x xstu
   have xs : x ∈ s := xstu.1.1
@@ -53,48 +59,96 @@ example : (s \ t) \ u ⊆ s \ (t ∪ u) := by
   constructor
   · exact xs
   intro xtu
+  show False
+
   -- x ∈ t ∨ x ∈ u
   rcases xtu with xt | xu
-  · show False; exact xnt xt
-  . show False; exact xnu xu
+  · exact xnt xt
+  · exact xnu xu
 
 example : (s \ t) \ u ⊆ s \ (t ∪ u) := by
   rintro x ⟨⟨xs, xnt⟩, xnu⟩
-  use xs
+  -- use xs  -- equivalent to refine ⟨ xs, ?_ ⟩
+
+  -- Using `refine`:
+  -- the placeholders ?_ are not unified, but extracted into new goals
+  refine ⟨ xs, ?_ ⟩
   rintro (xt | xu) <;> contradiction
 
 example : s \ (t ∪ u) ⊆ (s \ t) \ u := by
-  sorry
+  rintro x ⟨ hxs, hxntu ⟩
+  constructor
+  constructor
+  assumption
+  intro ht
+  exact hxntu (Or.inl ht)
+  intro hu
+  exact hxntu (Or.inr hu)
+  -- Proof automation also works!
+  -- `aesop` finds:
+  -- simp_all only [mem_union, not_or, mem_diff, not_false_eq_true, and_self]
+
 example : s ∩ t = t ∩ s := by
   ext x
   simp only [mem_inter_iff]
   constructor
   · rintro ⟨xs, xt⟩; exact ⟨xt, xs⟩
-  . rintro ⟨xt, xs⟩; exact ⟨xs, xt⟩
+  · rintro ⟨xt, xs⟩; exact ⟨xs, xt⟩
 
 example : s ∩ t = t ∩ s :=
-  Set.ext fun x ↦ ⟨fun ⟨xs, xt⟩ ↦ ⟨xt, xs⟩, fun ⟨xt, xs⟩ ↦ ⟨xs, xt⟩⟩
+  Set.ext fun _x ↦ ⟨fun ⟨xs, xt⟩ ↦ ⟨xt, xs⟩, fun ⟨xt, xs⟩ ↦ ⟨xs, xt⟩⟩
 
 example : s ∩ t = t ∩ s := by ext x; simp [and_comm]
 
 example : s ∩ t = t ∩ s := by
   apply Subset.antisymm
   · rintro x ⟨xs, xt⟩; exact ⟨xt, xs⟩
-  . rintro x ⟨xt, xs⟩; exact ⟨xs, xt⟩
+  · rintro x ⟨xt, xs⟩; exact ⟨xs, xt⟩
 
 example : s ∩ t = t ∩ s :=
-    Subset.antisymm sorry sorry
-example : s ∩ (s ∪ t) = s := by
-  sorry
+    Subset.antisymm
+      (fun _x ⟨ xs, xt ⟩ => ⟨ xt, xs ⟩)
+      (fun _x ⟨ xt, xs ⟩ => ⟨ xs, xt ⟩)
+
+example : s ∩ (s ∪ t) = s :=
+  Set.ext fun _x ↦ ⟨fun ⟨xs, _⟩ ↦ xs, fun xs ↦ ⟨xs, Or.inl xs⟩⟩
 
 example : s ∪ s ∩ t = s := by
-  sorry
+  ext
+  constructor
+  · rintro (hxs | hxst)
+    · assumption
+    · exact hxst.1
+  · rintro hxs
+    exact Or.inl hxs
 
 example : s \ t ∪ t = s ∪ t := by
-  sorry
+  ext x
+  constructor
+  · rintro (⟨hxs, _⟩ | hxt)
+    · exact Or.inl hxs
+    · exact Or.inr hxt
+  · rintro (hxs | hxt)
+    -- any other way than splitting on x ∈ t ?
+    · by_cases ht : x ∈ t
+      · exact Or.inr ht
+      · exact Or.inl ⟨hxs, ht⟩
+    · exact Or.inr hxt
 
 example : s \ t ∪ t \ s = (s ∪ t) \ (s ∩ t) := by
-  sorry
+  ext x
+  constructor
+  -- aesop + simplifications
+  · intro a
+    simp_all only [mem_union, mem_diff, mem_inter_iff, not_and]
+    cases a with
+    | inl h => simp_all only [or_false, not_false_eq_true, imp_self, and_self]
+    | inr h_1 => simp_all only [or_true, not_true_eq_false, false_implies, and_self]
+  · intro ⟨hleft, hright⟩
+    simp_all only [mem_diff, mem_union, mem_inter_iff, not_and]
+    cases hleft with
+    | inl h => simp_all only [true_implies, not_false_eq_true, and_self, not_true_eq_false, or_false]
+    | inr h_1 => simp_all only [not_true_eq_false, imp_false, and_self, not_false_eq_true, or_true]
 
 def evens : Set ℕ :=
   { n | Even n }
@@ -103,9 +157,9 @@ def odds : Set ℕ :=
   { n | ¬Even n }
 
 example : evens ∪ odds = univ := by
-  rw [evens, odds]
+  -- rw [evens, odds]
   ext n
-  simp
+  simp only [mem_union, mem_setOf_eq, mem_univ, iff_true]
   apply Classical.em
 
 example (x : ℕ) (h : x ∈ (∅ : Set ℕ)) : False :=
@@ -115,7 +169,13 @@ example (x : ℕ) : x ∈ (univ : Set ℕ) :=
   trivial
 
 example : { n | Nat.Prime n } ∩ { n | n > 2 } ⊆ { n | ¬Even n } := by
-  sorry
+  intro a ha
+  simp only [mem_inter_iff, mem_setOf_eq] at *
+  intro hne
+  rcases Nat.Prime.eq_two_or_odd ha.1 with (h2 | hodd)
+  · exact (Nat.ne_of_gt ha.2) h2
+  · have : a % 2 = 0 := Nat.even_iff.mp hne
+    exact one_ne_zero (Eq.trans hodd.symm this)
 
 #print Prime
 
@@ -135,6 +195,11 @@ end
 
 section
 
+-- Bounded quantifiers notation: `∀ x ∈ S, ...` and `∃ x ∈ S, ...`
+--
+-- Note: theorems with `ball_...` or `bex_...` stand for "bounded all", "bounded exists"
+#check bex_congr
+
 variable (s t : Set ℕ)
 
 example (h₀ : ∀ x ∈ s, ¬Even x) (h₁ : ∀ x ∈ s, Prime x) : ∀ x ∈ s, ¬Even x ∧ Prime x := by
@@ -151,10 +216,15 @@ section
 variable (ssubt : s ⊆ t)
 
 example (h₀ : ∀ x ∈ t, ¬Even x) (h₁ : ∀ x ∈ t, Prime x) : ∀ x ∈ s, ¬Even x ∧ Prime x := by
-  sorry
+  intro x xs
+  constructor
+  · exact h₀ x (ssubt xs)
+  exact h₁ x (ssubt xs)
 
 example (h : ∃ x ∈ s, ¬Even x ∧ Prime x) : ∃ x ∈ t, Prime x := by
-  sorry
+  rcases h with ⟨x, hxs, _, hpx⟩
+  use x
+  exact ⟨ssubt hxs, hpx⟩
 
 end
 
@@ -162,7 +232,7 @@ end
 
 section
 variable {α I : Type*}
-variable (A B : I → Set α)
+variable (A B : I → Set α)  -- indexed family of sets A_i, B_i
 variable (s : Set α)
 
 open Set
@@ -193,19 +263,41 @@ example : (⋂ i, A i ∩ B i) = (⋂ i, A i) ∩ ⋂ i, B i := by
 
 
 example : (s ∪ ⋂ i, A i) = ⋂ i, A i ∪ s := by
-  sorry
+  ext x
+  simp only [mem_union, mem_iInter]
+  constructor
+  · rintro (xs | xAi)
+    · intro
+      exact Or.inr xs
+    intro i
+    exact Or.inl (xAi i)
+  -- ← requires classical logic
+  intro h
+  by_cases xs : x ∈ s
+  · exact Or.inl xs
+  right
+  intro i
+  rcases h i
+  · assumption
+  contradiction
+
 
 def primes : Set ℕ :=
   { x | Nat.Prime x }
 
+-- These examples are hard to read: | is different than ∣
 example : (⋃ p ∈ primes, { x | p ^ 2 ∣ x }) = { x | ∃ p ∈ primes, p ^ 2 ∣ x } :=by
   ext
   rw [mem_iUnion₂]
-  simp
+  -- simp
+  -- alternatively:
+  -- don't exactly understand `exists_prop`. "Exists a proof of a, s.t. b holds ↔ a ∧ b"?
+  simp only [mem_setOf_eq, exists_prop]
 
 example : (⋃ p ∈ primes, { x | p ^ 2 ∣ x }) = { x | ∃ p ∈ primes, p ^ 2 ∣ x } := by
   ext
-  simp
+  -- simp
+  simp only [mem_iUnion, mem_setOf_eq, exists_prop]
 
 example : (⋂ p ∈ primes, { x | ¬p ∣ x }) ⊆ { x | x = 1 } := by
   intro x
@@ -214,7 +306,13 @@ example : (⋂ p ∈ primes, { x | ¬p ∣ x }) ⊆ { x | x = 1 } := by
   apply Nat.exists_prime_and_dvd
 
 example : (⋃ p ∈ primes, { x | x ≤ p }) = univ := by
-  sorry
+  rw [eq_univ_iff_forall]
+  intro x
+  simp only [mem_iUnion₂, exists_prop]
+  rcases Nat.exists_infinite_primes x with ⟨q, hxq, hq'⟩
+  use q
+  exact ⟨hq', hxq⟩
+
 
 end
 
@@ -235,4 +333,3 @@ example : ⋂₀ s = ⋂ t ∈ s, t := by
   rfl
 
 end
-
