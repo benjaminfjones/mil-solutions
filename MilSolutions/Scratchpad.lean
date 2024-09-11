9921f18c171387896eac0a387d70a4b2814ef998
Benjamin Jones <benjaminfjones@gmail.com>
2024-09-11 21:02:37 +0000

    Add FermatRecurrence to Scratchpad

--- MilSolutions/Scratchpad.lean
+++ MilSolutions/Scratchpad.lean
@@ -1,4 +1,6 @@
+import Mathlib.Algebra.BigOperators.Group.Finset
 import Mathlib.Data.Finset.Basic
+import Mathlib.Tactic
 
 namespace Mark
 open Finset
@@ -7,7 +9,7 @@ def Address := Finset.range 1024
 example : 0 ∈ Address := by
   unfold Address
   rw [mem_range]
-  norm_num
+  simp only [Nat.zero_lt_succ]
 
 end Mark
 
@@ -21,3 +23,26 @@ example (e f: α) [BEq α] [LawfulBEq α]: (e == f) = true ↔ e = f := by
 
 end Sean
 
+namespace Fermat
+-- import Mathlib.Algebra.BigOperators.Group.Finset
+-- import Mathlib.Data.Finset.Basic
+-- import Mathlib.Tactic
+
+open Finset
+
+def Fermat (n : Nat) : Nat := 2^(2^n) + 1
+
+#check Finset.prod
+
+theorem FermatRecurrence {n : Nat} : Finset.prod (range (n+1)) (fun i => Fermat i) = Fermat (n+1) - 2 := by
+  induction' n with k ih
+  · simp only [zero_add, range_one, prod_singleton]
+    unfold Fermat
+    norm_num
+  · -- use relationship b/w range (k+1) and range (k + 1 + 1)
+    sorry
+
+theorem FermatRecurrence' {n : Nat} : ∏ i ∈ range (n+1), Fermat i = Fermat (n+1) - 2 := by
+  sorry
+
+end Fermat
